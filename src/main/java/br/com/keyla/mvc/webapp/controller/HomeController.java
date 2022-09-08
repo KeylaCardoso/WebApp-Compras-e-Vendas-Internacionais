8c9ab4e39bf78d8532f77f2f9643d58e185470b7
Keyla <keylarezendec@gmail.com>
2022-09-08 18:21:14 -0300

    Atualização para utilização do projeto Spring Data JPA

--- src/main/java/br/com/keyla/mvc/webapp/controller/HomeController.java
+++ src/main/java/br/com/keyla/mvc/webapp/controller/HomeController.java
@@ -2,32 +2,28 @@ package br.com.keyla.mvc.webapp.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.keyla.mvc.webapp.model.Pedido;
+import br.com.keyla.mvc.webapp.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
-	
+	@Autowired //serve para "solicitar" ao String uma instancia de PedidoRepository, no caso 
+	private PedidoRepository pedidoRepository;
+		
 	@GetMapping("/home")
 	public String home(Model model) {
 		
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		List<Pedido> pedidos = query.getResultList();		
-		
-		
+		List<Pedido> pedidos = pedidoRepository.findAll();		
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 		
 	}
+	
 
 }
